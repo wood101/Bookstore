aec8364f22d71ff1847f3b73426f1afde540899a
wood101 <janne.malin@hotmail.com>
2017-02-02 15:32:01 +0200

    Korjattu

--- src/main/java/fi/haaga/helia/Bookstore/controller/BookController.java
+++ src/main/java/fi/haaga/helia/Bookstore/controller/BookController.java
@@ -17,7 +17,7 @@ public class BookController {
 		@Autowired
 		private BookRepository repository;
 		
-		@RequestMapping("/")
+		@RequestMapping(value = "/")
 		public String booklist(Model model) {
 			 List<Book> books = (List<Book>) repository.findAll();
 			
